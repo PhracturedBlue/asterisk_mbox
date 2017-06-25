2f62046bb34a2fab93b20f557487ebed89a1bc36
PhracturedBlue <rc2012@pblue.org>
2017-06-25 08:34:58 -0700

    Try to get README working with pip

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 setup(
   name = 'asteriskvm',
   packages=find_packages(),
-  version = '0.1.5',
+  version = '0.1.6',
   description = 'A client/server to interact with Asterisk voicemail mailboxes',
+  long_description=open('README.rst').read(),
   author = 'PhracturedBlue',
   author_email = 'rc2012@pblue.org',
   url = 'https://github.com/PhracturedBlue/asteriskvm', # use the URL to the github repo
