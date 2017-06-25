c315932e74aae7729fae99eb5443666dba0a5808
PhracturedBlue <rc2012@pblue.org>
2017-06-25 08:30:38 -0700

    Updates for build process

--- README.rst
+++ README.rst
@@ -1,21 +1,26 @@
-# AsteriskVM
-
+==========
+AsteriskVM
+==========
 AsteriskVM is a client/server which provides an API to work with Asterisk voicemail mailboxes
 
-## Dependencies
+Dependencies
+============
 
 * Linux ≥ 2.6.13
 * Python ≥ 3.4
 * Lame
 
-## Capabilities
+Capabilities
+============
 
 * Display all metadata for all messages stored in a single mailbox
 * Convert messages to text using Google's speech-to-text service
 * Retrieve the audio of a message in MP3 format
 * Support both synchronous and asynchronous API
 
-## Future work
+Future work
+============
+
 * Support deleting messages
 * Support moving messages
 * Support multiple mailboxes
--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
   name = 'asteriskvm',
-  packages = ['asteriskvm'], # this must be the same as the name above
-  version = '0.1',
+  packages=find_packages(),
+  version = '0.1.5',
   description = 'A client/server to interact with Asterisk voicemail mailboxes',
   author = 'PhracturedBlue',
   author_email = 'rc2012@pblue.org',
   url = 'https://github.com/PhracturedBlue/asteriskvm', # use the URL to the github repo
-  download_url = 'https://github.com/PhracturedBlue/asteriskvm/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'asterisk'], # arbitrary keywords
   classifiers = [],
+  install_requires=[
+        "inotify",
+        "SpeechRecognition",
+   ],
+   entry_points = {
+        'console_scripts': [
+            'asteriskvm-server = asteriskvm.server:main'
+        ]
+   },
+
 )
