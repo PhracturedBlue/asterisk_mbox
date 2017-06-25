6068d52b2f62c648daac0a76941c44c0f0eaac7a
PhracturedBlue <rc2012@pblue.org>
2017-06-25 07:29:46 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'asteriskvm',
+  packages = ['asteriskvm'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A client/server to interact with Asterisk voicemail mailboxes',
+  author = 'PhracturedBlue',
+  author_email = 'rc2012@pblue.org',
+  url = 'https://github.com/PhracturedBlue/asteriskvm', # use the URL to the github repo
+  download_url = 'https://github.com/PhracturedBlue/asteriskvm/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'asterisk'], # arbitrary keywords
+  classifiers = [],
+)
